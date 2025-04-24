64147c460f143b0cd43a875fba73636bd83ed7e5
SveTi4 <studymail2.7.53@gmail.com>
2025-04-24 14:46:38 +0700

    Add deleteButton component

---
+++ src/components/atoms/DeleteButton/DeleteButton.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+import { TrashIcon } from '../Icon/icons'
+
+const StyledButton = styled.button<{ $size?: number }>`
+  background: none;
+  border: none;
+  padding: 4px;
+  color: ${({ theme }) => theme.colors.textSecondary};
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.6;
+  transition: all 0.2s ease;
+
+  &:hover {
+    opacity: 1;
+    color: ${({ theme }) => theme.colors.error};
+  }
+
+  svg {
+    width: ${({ $size }) => $size}px;
+    height: ${({ $size }) => $size}px;
+    stroke: currentColor;
+  }
+`
+
+interface DeleteButtonProps {
+  onClick: (e: React.MouseEvent) => void;
+  size?: number;
+}
+
+export const DeleteButton: React.FC<DeleteButtonProps> = ({ onClick, size = 16 }) => {
+  return (
+    <StyledButton onClick={onClick} $size={size}>
+      <TrashIcon size={size} />
+    </StyledButton>
+  )
+} 
