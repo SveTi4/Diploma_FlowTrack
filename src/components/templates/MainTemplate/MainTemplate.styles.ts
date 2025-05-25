357f8d47377d242e9fab2278f13813d11d25c99f
SveTi4 <studymail2.7.53@gmail.com>
2025-05-25 13:04:17 +0700

    Move MainTemplate styles to a separate file

---
+++ src/components/templates/MainTemplate/MainTemplate.styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display: flex;
+    height: 100vh;
+    background: ${({ theme }) => theme.colors.surface};
+    color: ${({ theme }) => theme.colors.text};
+`
+
+export const Content = styled.main`
+    flex: 1;
+    background: ${({ theme }) => theme.colors.background};
+    height: 100vh;
+    overflow-y: scroll;
+
+    /* Стилизация скроллбара */
+    &::-webkit-scrollbar {
+        width: 8px;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: ${({ theme }) => theme.colors.surface};
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background: ${({ theme }) => theme.colors.border};
+        border-radius: ${({ theme }) => theme.borderRadius.small};
+    }
+
+    &::-webkit-scrollbar-thumb:hover {
+        background: ${({ theme }) => theme.colors.surfaceHover};
+    }
+`
+export const ThemeToggleWrapper = styled.div`
+    position: absolute;
+    right: 48px;
+    bottom: 24px;
+`
--- src/components/templates/MainTemplate/MainTemplate.tsx
+++ src/components/templates/MainTemplate/MainTemplate.tsx
@@ -1,43 +1,6 @@
-import styled from 'styled-components'
 import { Sidebar } from '../../organisms/Sidebar/Sidebar'
 import {ThemeToggle} from "../../atoms/ThemeToggle/ThemeToggle.tsx";
-
-const Container = styled.div`
-    display: flex;
-    height: 100vh;
-    background: ${({ theme }) => theme.colors.surface};
-    color: ${({ theme }) => theme.colors.text};
-`
-
-const Content = styled.main`
-    flex: 1;
-    background: ${({ theme }) => theme.colors.background};
-    height: 100vh;
-    overflow-y: scroll;
-
-    /* Стилизация скроллбара */
-    &::-webkit-scrollbar {
-        width: 8px;
-    }
-
-    &::-webkit-scrollbar-track {
-        background: ${({ theme }) => theme.colors.surface};
-    }
-
-    &::-webkit-scrollbar-thumb {
-        background: ${({ theme }) => theme.colors.border};
-        border-radius: ${({ theme }) => theme.borderRadius.small};
-    }
-
-    &::-webkit-scrollbar-thumb:hover {
-        background: ${({ theme }) => theme.colors.surfaceHover};
-    }
-`
-const ThemeToggleWrapper = styled.div`
-    position: absolute;
-    right: 48px;
-    bottom: 24px;
-`
+import { Container, Content, ThemeToggleWrapper } from './MainTemplate.styles.ts';
 
 interface MainTemplateProps {
   children: React.ReactNode
