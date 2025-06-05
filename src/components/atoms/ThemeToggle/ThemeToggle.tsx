bd6f5c11cf5e837089eda977d82c84441876f07e
SveTi4 <studymail2.7.53@gmail.com>
2025-06-06 00:51:30 +0700

    Move svg Theme icons in icons

--- src/components/atoms/ThemeToggle/ThemeToggle.tsx
+++ src/components/atoms/ThemeToggle/ThemeToggle.tsx
@@ -1,5 +1,6 @@
 import { useTheme } from '../../../contexts/ThemeContext'
 import { ToggleButton } from './ThemeToggle.styles'
+import {ThemeDarkIcon, ThemeLightIcon} from "../Icon/icons.tsx";
 
 export const ThemeToggle = () => {
   const { theme, toggleTheme } = useTheme()
@@ -7,14 +8,9 @@ export const ThemeToggle = () => {
   return (
     <ToggleButton onClick={toggleTheme} aria-label="Переключить тему">
       {theme === 'light' ? (
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
-        </svg>
+        <ThemeLightIcon />
       ) : (
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-          <circle cx="12" cy="12" r="5" />
-          <path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42" />
-        </svg>
+        <ThemeDarkIcon />
       )}
     </ToggleButton>
   )
