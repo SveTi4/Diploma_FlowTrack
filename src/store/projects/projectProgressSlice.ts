fb2820b129f893021fea25ac9b0d97fde3423d9e
SveTi4 <studymail2.7.53@gmail.com>
2025-06-07 19:49:25 +0700

    Fix import

--- src/store/projects/projectProgressSlice.ts
+++ src/store/projects/projectProgressSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { services } from '../../api/services'
-import { ProjectProgress, ProjectBurndownData } from '../../api/services/projects/types'
+import { ProjectProgress, ProjectBurndownData } from '../../api/services'
 import { ApiResponse } from '../../api/types/response.types'
 
 interface ProjectProgressState {
