92511374f219a6460956a74ae0f9bf4f71c16db2
SveTi4 <studymail2.7.53@gmail.com>
2025-06-13 00:30:19 +0700

    Update StatsPage

---
+++ src/components/pages/StatsPage/StatsPage.styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  padding: 24px;
+`;
+
+export const ChartsContainer = styled.div`
+  display: flex;
+  gap: 24px;
+  width: 100%;
+`;
+
+export const ChartWrapper = styled.div`
+  flex: 1;
+  min-width: 0;
+`; 
--- src/components/pages/StatsPage/StatsPage.tsx
+++ src/components/pages/StatsPage/StatsPage.tsx
@@ -1,5 +1,47 @@
-import { PagePlaceholder } from '../../atoms'
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../../store';
+import { fetchHeatmap } from '../../../store/heatmap/heatmapSlice.ts';
+import { PageHeader } from '../../molecules';
+import { HeatmapChart } from './components/HeatmapChart/HeatmapChart.tsx';
+import { WeekdayActivityChart } from './components/WeekdayActivityChart/WeekdayActivityChart.tsx';
+import { MonthlyActivityChart } from './components/MonthlyActivityChart/MonthlyActivityChart.tsx';
+import { Container, Content, ChartsContainer, ChartWrapper } from './StatsPage.styles';
 
-export const StatsPage = () => (
-  <PagePlaceholder title="Статистика" />
-) 
+export const StatsPage = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { data: heatmapData, loading, error } = useSelector((state: RootState) => state.heatmap);
+
+  useEffect(() => {
+    dispatch(fetchHeatmap());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Загрузка...</div>;
+  }
+
+  if (error) {
+    return <div>Ошибка: {error}</div>;
+  }
+
+  if (!heatmapData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <PageHeader title="Статистика" />
+      <Content>
+        <HeatmapChart data={heatmapData} />
+        <ChartsContainer>
+          <ChartWrapper>
+            <WeekdayActivityChart data={heatmapData} />
+          </ChartWrapper>
+          <ChartWrapper>
+            <MonthlyActivityChart data={heatmapData} />
+          </ChartWrapper>
+        </ChartsContainer>
+      </Content>
+    </Container>
+  );
+}; 
